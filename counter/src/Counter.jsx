c4fe3b86873d14d7b193cf6466366ba93739abca
COM <meneghini.ornella@gmail.com>
2022-06-22 08:54:22 -0300

    update

--- counter/src/Counter.jsx
+++ counter/src/Counter.jsx
@@ -5,7 +5,7 @@ function Counter({ value }) {
   const [counter, setCounter] = useState(value);
   return (
     <div className="App">
-      <h1>Counter: <span className='counter'>{ counter }</span></h1>
+      <h1>Counter: <span>{ counter }</span></h1>
       <div>
         <button onClick={ () => setCounter(counter +1) }>+ 1</button>
         <button onClick={ () => setCounter(value) }>Reset</button>
